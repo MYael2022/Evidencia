bdde54b36d0aff49ab77a99a4b24be77ee1dcc58
Reymundo Lozano <rayolo_puma@live.com.mx>
2021-10-29 12:45:55 -0500

    clases R

--- Paciente.java
+++ Paciente.java
@@ -1,3 +1,11 @@
 public class Paciente {
+    int IdUsuario;
+    string Nombre;
+    string Apellido;
+    int Telefono;
+    string Direccion;
+    int Edad;
+    string Sexo;
+    string Motivo;
     
 }
